f7d159017ad0acd9979c4941aa4918a5b4719793
Willy Fajar Ramadhan <willyfr88@gmail.com>
2022-06-05 08:58:48 +0700

    adding error state

--- script/app.js
+++ script/app.js
@@ -1,13 +1,18 @@
-// function validate() {
-//     var mail = document.getElementById("email").value
-//     var regs = /^([a-zA-Z0-9\._]+)@([a-a-zA-Z0-9])+.([a-z]+)(.[a-z]+)?$/
+const form = document.querySelector('.form');
+const email = document.querySelector('.email');
 
-//     if(regx.text(mail)) {
-//         alert("Email valid")
-//         return true;
-//     }
-//     else {
-//         alert("Email invalid")
-//         return false;
-//     }
-// }
+function validateEmail (email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+};
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	let emailValue = email.value;
+	console.log(email.value);
+	if (validateEmail(emailValue)) {
+		form.classList.remove('error');
+	} else {
+		form.classList.add('error');
+	}
+});
